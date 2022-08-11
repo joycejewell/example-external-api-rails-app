2cd0609e66ad40ad4c5dbb723b6f56ae408fa91d
joycejewell <joyceroop@gmail.com>
2022-08-10 18:22:10 -0600

    added github controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
  get "/newsapi_headlines" => "pages#newsapi_headlines"
-end
+ get "/auth/github/callback" => "git_hub#authorize"
+ get "/github_profile" => "git_hub#profile"
+ end
